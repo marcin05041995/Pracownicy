1c355b5e5b5c1b072f896d9df1f84d6b2238a84a
marcin05041995 <marcin05041995@onet.pl>
2018-07-25 04:30:00 -0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img src="./assets/pracownik.jpg">
+    <Worker/>
+    <AddWorker/>
+  </div>
+
+
+
+</template>
+
+<script>
+import Worker from './components/Worker'
+import AddWorker from './components/AddWorker'
+
+
+export default {
+  name: 'App',
+  components: {
+    Worker,AddWorker
+  }
+}
+</script>
+
+<style>
+
+template{
+  background-color: #2c3e50;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+#app>img{
+  width:300px;
+  height:300;
+  float:left;
+  margin-left:100px;
+}
+</style>
---
+++ src/components/AddWorker.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="AddWorker">
+  <div style=clear:both;></div>
+  <h1>{{Message}}</h1>
+    <button class="btn btn-primary">Dodaj pracownika</button>
+    <button class="btn btn-success">Edytuj</button>
+    <button class="btn btn-info">Szczegóły</button>
+    <button class="btn btn-danger">Usuń</button>
+
+      <form>
+        <div class="well">
+            <h4>Dane pracownika</h4>
+            <div class="form-group">
+                <label>Imie</label>
+                <input type="text" class="form-control"  placeholder="Podaj imie" >
+            </div>
+            <div class="form-group">
+                <label>Nazwisko</label>
+                <input type="text" class="form-control"  placeholder="Podaj nazwisko" >
+            </div>
+            <div class="form-group">
+                <label>Data urodzenia</label>
+                <input type="text" class="form-control"  placeholder="Podaj date" >
+            </div>
+            <div class="form-group">
+                <label>Numer telefonu</label>
+                <input type="text" class="form-control"  placeholder="Podaj numer" >
+            </div>
+
+        </div>
+    </form>
+</div>
+
+
+</template>
+
+<script>
+export default{
+  name: 'AddWorker',
+  data(){
+    return{
+      Message: 'Wybierz akcję której chcesz dokonać.'
+    }
+  }
+}
+</script>
+<style>
+.btn{
+  width:150px;
+  margin-top:10px;
+}
+.well{
+  margin-top:10px;
+}
+  .form-group{
+    width:25%;
+    margin-left:auto;
+    margin-right:auto;
+  }
+</style>
